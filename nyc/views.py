8e7180b53480c5686bb4b6a9ebde133d7459510e
manny2trill <manuelr42787@gmail.com>
2021-11-13 00:44:49 -0600

    mannys first push

--- nyc/views.py
+++ nyc/views.py
@@ -15,5 +15,6 @@ def activity(request, borough,activity):
         return render(request=request, template_name='activity.html', context={ 'borough': borough, 'activity': activity, 'activities': boroughs[borough][activity].keys()})
 
 
-def venue():
-    pass
+def venue(request, borough, activity, venue):
+    if request.method == 'GET':
+        return render(request=request, template_name='venue.html', context={'borough': borough,'activity': activity, 'venue': venue, 'description': boroughs[borough][activity][venue].get('description')})
